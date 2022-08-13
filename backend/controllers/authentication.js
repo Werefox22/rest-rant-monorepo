860cb0a4242ede400fa0e6492128901401522e82
Werefox <jack@wrightus.com>
2022-08-13 10:51:27 -0400

    Implemented cors

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -11,29 +11,33 @@ router.post('/', async (req, res) => {
         where: { email: req.body.email }
     })
 
-    console.log(user)
+    // console.log(user)
 
     if (!user || !await bcrypt.compare(req.body.password, user.password_digest)) {
         res.status(404).json({
             message: `Could not find a user with the provided username and password`
         })
     } else {
+        // successfully logged in
+        req.session.userId = user.userId
         res.status(200).json({ user })
     }
 })
 
 // get current user
-// router.get('/profile', async (req, res) => {
-//     try {
-//         let user = await User.findOne({
-//             where: {
-//                 userId: '?'
-//             }
-//         })
-//         res.json(user)
-//     } catch {
-//         res.json(null)
-//     }
-// })
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId)
+
+    try {
+        // let user = await User.findOne({
+        //     where: {
+        //         userId: '?'
+        //     }
+        // })
+        // res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
 
 module.exports = router
