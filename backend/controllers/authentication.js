0e318bc0b216a14dfb5da8600307746b94cc6410
Werefox <jack@wrightus.com>
2022-08-13 11:21:11 -0400

    Implemented JWT processing to the getCurrentUser endpoint

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
-const jwt = require('jwt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -28,12 +28,20 @@ router.post('/', async (req, res) => {
 // get current user
 router.get('/profile', async (req, res) => {
     try {
-        // let user = await User.findOne({
-        //     where: {
-        //         userId: '?'
-        //     }
-        // })
-        // res.json(user)
+        const [ authenticationMethod, token ] = req.headers.authorization.split(' ')
+
+        if (authenticationMethod == 'Bearer') {
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            const { id } = result.value
+
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
     } catch {
         res.json(null)
     }
