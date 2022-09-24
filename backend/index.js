f4842b8e7df7b81ceb7473ef7b22bdb4a5a536a1
Werefox <jack@wrightus.com>
2022-09-24 11:18:33 -0400

    Rebuilt the frontend and corrected the production url so the server will use it

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client', 'build')))
+    app.use(express.static(path.join(__dirname, 'public', 'build')))
 }
 
 // Controllers & Routes
